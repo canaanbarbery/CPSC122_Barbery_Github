28533db063f840579fe96943bc60a006c605516b
Canaan Barbery <cbarbery@zagmail.gonzaga.edu>
2026-01-22 12:19:58 -0800

    image modifer

--- image_modifier.py
+++ image_modifier.py
@@ -7,57 +7,91 @@ def pretty_print(data):
     for row in data:
         print(row)
 
-def load_image_data(image: str):
+def load_image_data(filename: str):
     row = []
     data = []
     x = 0
-    with open(image, "r") as infile:
+    with open(filename, "r") as infile:
         for line in infile:
             line = line.strip()
             x += 1
             if x < 4:
                 continue
             vals = line.split(" ")
-            for i in range(len(vals)):
-                vals[i] = int(vals[i])
             
             for i in range(0,len(vals), 3):
-                red = vals[i]
-                green = vals[i +1]
-                blue = vals[i+2]
+                red = int(vals[i])
+                green = int(vals[i + 1])
+                blue = int(vals[i + 2])
                 pixel = [red,green,blue]
                 row.append(pixel)
             data.append(row)
             row = []
-        pretty_print(data)
+
+    return data 
+
+def write_image_data(data: list[list[list[int]]], filename: str):
     
-    return data
-            
-def vertical_flip():
+    with open(filename, "w") as outfile:
+        for row in data:
+            for pixel in row:
+                for x in pixel:
+                    x = str(x)
+                    outfile.write(x+" ")
+            outfile.write("\n")
+
+
+def apply_modification(data: list[list[list[int]]], mod: str):
+    if mod == "a":
+        
+        data = vertical_flip(data)
+        filename = ("vflip_"+filename)
+        print(filename)
+        write_image_data(data, filename)
+        
+        
+
+    
+    return data, filename
+
+def vertical_flip(data: list[list[list[int]]]):
+    new_data = []
+    rounds = 1
+    for i in range(0,len(data)):
+        for row in range(0,len(data)):
+            if row == (len(data) - rounds):
+                new_data.append(data[row])
+                rounds += 1
+    
+    return new_data
 
 def start_menu():
-    
-    output_file = "new_image.ppm"
+    exit_pgm = False
     # input_file = input("What image would you like to use? ")    
-    input_file = "test.ppm"
+    image_file = "test.ppm"
 
-    image_data = load_image_data(input_file)
+    image_data = load_image_data(image_file)
     
-    
-    user_input = input("How would you like to edit your image?\n"
-                       "\n"
-                       "a) Flip the image vertically\n"
-                       "b) Flip the image horizontally\n"
-                       "c) Remove a primary color (red, green, blue)\n"
-                       "d) Negate the colors\n"
-                       "e) Apply a high contrast\n"
-                       "f) Add random noise\n"
-                       "g) Apply a gray scale\n"
-                       "h) Apply a horizontal blur\n"
-                       "i) Save and exit\n"
-                       "\n"
-                       "> ")
-    if user_input == "a":
-       image_data = vertical_flip(image_data)
-    
+    while exit_pgm != True:
+        user_input = input("How would you like to edit your image?\n"
+                        "\n"
+                        "a) Flip the image vertically\n"
+                        "b) Flip the image horizontally\n"
+                        "c) Remove a primary color (red, green, blue)\n"
+                        "d) Negate the colors\n"
+                        "e) Apply a high contrast\n"
+                        "f) Add random noise\n"
+                        "g) Apply a gray scale\n"
+                        "h) Apply a horizontal blur\n"
+                        "i) Save and exit\n"
+                        "\n"
+                        "> ")
+        
+        if user_input == "i":
+            write_image_data(image_data, image_file)
+            print("Image saved.")
+            exit_pgm = True
+        apply_modification(user_input)
+        
+        
 start_menu()
