9892136f0e96675896244143bdd33650e2727dfb
Canaan Barbery <cbarbery@zagmail.gonzaga.edu>
2026-01-22 20:00:21 -0800

    image modifier

--- image_modifier.py
+++ image_modifier.py
@@ -45,53 +45,155 @@ def apply_modification(data: list[list[list[int]]], mod: str):
     if mod == "a":
         
         data = vertical_flip(data)
-        filename = ("vflip_"+filename)
-        print(filename)
-        write_image_data(data, filename)
+
+    elif mod == "b":
+            
+        data = horizontal_flip(data)
+        
+    # elif mod == "c":
+     
+
+    # elif mod == "d":
+        
+    
+
+    # elif mod == "e":
+        
+   
+    
+    # elif mod == "f":
+        
+       
+    
+    # elif mod == "g":
+        
+        
+    
+    # elif mod == "h":
+        
+   
+    
+    # elif mod == "i":
+        
+     
+    
+    # elif mod == "j":
+        
         
         
 
     
-    return data, filename
+    return data 
 
 def vertical_flip(data: list[list[list[int]]]):
     new_data = []
-    rounds = 1
     for i in range(0,len(data)):
-        for row in range(0,len(data)):
-            if row == (len(data) - rounds):
-                new_data.append(data[row])
-                rounds += 1
-    
+        new_data.append(data[len(data)-1-i])
+    pretty_print(new_data)
+    return new_data
+
+def horizontal_flip(data: list[list[list[int]]]):
+    new_data = []
+    new_row = []
+    for row in range(0,len(data)):
+        for pixel in range(0, len(data[row])):
+            new_row.append(data[row][len(data[row])-1-pixel])
+        new_data.append(new_row)
+        new_row = []
+    pretty_print(new_data)
+
     return new_data
 
 def start_menu():
     exit_pgm = False
-    # input_file = input("What image would you like to use? ")    
-    image_file = "test.ppm"
-
-    image_data = load_image_data(image_file)
+    # input_file = input("What image would you like to use? ")     
     
+    input_file = "test.ppm"
+    filename = input_file
+
+    image_data = load_image_data(input_file)
+    pretty_print(image_data)
     while exit_pgm != True:
         user_input = input("How would you like to edit your image?\n"
                         "\n"
                         "a) Flip the image vertically\n"
                         "b) Flip the image horizontally\n"
-                        "c) Remove a primary color (red, green, blue)\n"
-                        "d) Negate the colors\n"
-                        "e) Apply a high contrast\n"
-                        "f) Add random noise\n"
-                        "g) Apply a gray scale\n"
-                        "h) Apply a horizontal blur\n"
-                        "i) Save and exit\n"
+                        "c) Remove red\n"
+                        "d) Remove green\n"
+                        "e) Remove blue\n"
+                        "f) Negate the colors\n"
+                        "g) Apply a high contrast\n"
+                        "h) Add random noise\n"
+                        "i) Apply a gray scale\n"
+                        "j) Apply a horizontal blur\n"
+                        "k) Save and exit\n"
                         "\n"
                         "> ")
         
-        if user_input == "i":
-            write_image_data(image_data, image_file)
-            print("Image saved.")
+        if user_input == "a":
+            
+            image_data = apply_modification(image_data, user_input)
+            filename = "vflip_" + filename
+            write_image_data(image_data, filename)
+        
+        elif user_input == "b":
+            
+            filename = ("hflip_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+        
+        elif user_input == "c":
+            
+            filename = ("rmred_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+
+        elif user_input == "d":
+            
+            filename = ("rmgreen_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+
+        elif user_input == "e":
+            
+            filename = ("rmblue_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+        
+        elif user_input == "f":
+            
+            filename = ("neg_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+        
+        elif user_input == "g":
+            
+            filename = ("hcont_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+        
+        elif user_input == "h":
+            
+            filename = ("noise_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+        
+        elif user_input == "i":
+            
+            filename = ("bw_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+        
+        elif user_input == "j":
+            
+            filename = ("blur_"+filename)
+            image_data = apply_modification(image_data, user_input)
+            write_image_data(image_data, filename)
+        
+        elif user_input == "k":
+            print("Exiting program...")
             exit_pgm = True
-        apply_modification(user_input)
+        
         
         
 start_menu()
